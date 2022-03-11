8cd208f2ba34e22e88a30e56b201738214de6036
Muhamad Nizam <81418686+callmezamm@users.noreply.github.com>
2022-03-11 13:34:32 +0700

    menambahkan baris baru di file fungsi.php

--- TUGAS 1/fungsi.php
+++ TUGAS 1/fungsi.php
@@ -7,4 +7,5 @@
     echo "umur saya sekarang ". hitungUmur(2002,2022). " tahun ";
     echo " sedangkan umur doni sekarang ". hitungUmur(2001,2022). " tahun ";
     echo " dan umur rani sekarang ". hitungUmur(2003,2022). " tahun ";
+    echo " dan umur siska sekarang" . hitungUmur(2004,2022). " tahun ";
 ?>
